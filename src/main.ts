426f1304771976892d12a7417649ea2eee96750b
NoodleOfDeath <thom@noodleofdeath.com>
2022-04-30 16:23:32 -0400

    adds vuex

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false;
 
@@ -9,6 +10,7 @@ new Vue({
   vuetify,
   render: (h) => h(App),
   router,
+  store,
   watch: {
     $route: {
       immediate: true,
